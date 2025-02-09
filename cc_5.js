8d751332b66871ce7fa1f878bbbe2f540474b80c
Natalie Devenbeck <devenbeck@usf.edu>
2025-02-09 09:16:06 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,11 @@
+// Task 1: Object Properties
+//Scenario: Customer Profile
+const customer = {    //customer object created
+    name: "John Doe", //beginning of listed properties
+    age: 35,
+    email: "john.doe@email.com"
+}; //finalized object
+
+console.log(`Name: ${customer.name}`); //logging first property with a template literal
+console.log(`Age: ${customer.age}`); //logging second property with a template literal
+console.log(`Email: ${customer.email}`); //logging third property with a template literal
