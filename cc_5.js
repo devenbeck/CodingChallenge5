22b3aeafeadf5d1a07bae42e69798da8e75bbdfc
Natalie Devenbeck <devenbeck@usf.edu>
2025-02-09 09:24:25 -0500

    Task 2- Order Details

--- cc_5.js
+++ cc_5.js
@@ -9,3 +9,15 @@ const customer = {    //customer object created
 console.log(`Name: ${customer.name}`); //logging first property with a template literal
 console.log(`Age: ${customer.age}`); //logging second property with a template literal
 console.log(`Email: ${customer.email}`); //logging third property with a template literal
+
+//Task 2: Object Methods
+//Scenario: Order Details
+const order = {           //object order declared
+    orderID: 12345,       //beginning of properties listed     
+    totalAmount: 150,
+    status: "Processing",
+    displayOrder() {     // adding a display order method to log details 
+        console.log(`Order ID: ${this.orderID}, Total Amount: $${this.totalAmount}, Status: ${this.status}`);
+    }
+}; //log of order with details
+order.displayOrder();
